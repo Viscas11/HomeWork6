52eb7adbe5cfd93a893e0776c660fce19c595ec1
Syat <titmouse99@mail.ru>
2023-04-26 00:52:41 +0300

    1,2,3 Задача

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int salary = 15_000;
+        int total = 0;
+        int month = 0;
+        while (total <= 2_459_000) {
+            month = month + 1;
+            total = total + salary;
+            System.out.println(" Месяц " + month + ", сумма накоплений равна " + total + " Рублей ");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int start = 0;
+        while (start < 10) {
+            start = start + 1;
+            System.out.print(start + " ");
+        }
+        System.out.println();
+        int i = 11;
+        for (; i > 1; ) {
+            i = i - 1;
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int people = 12_000_000;
+        int natalityPer1000 = 17;
+        int dethPer1000 = 8;
+        int year = 0;
+        while (year < 10) {
+            int populationPerYear = people / 1000 * (natalityPer1000 - dethPer1000);
+            people = people + populationPerYear;
+          year = year + 1;
+            System.out.println(" Год " + year + ", численность населения составляет " + people);
+        }
+    }
+}
